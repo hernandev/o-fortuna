0b683a4f09fa8857795c1cd2b632fa25cff128f7
Diego Hernandes <diego@hernandev.com>
2018-02-24 02:49:39 -0300

    bank sip and credit card issuer interface

---
+++ src/Contracts/Issuers/BankSlipIssuer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace O\Fortuna\Contracts\BankSlips;
+
+/**
+ * Interface BankSlipIssuer.
+ *
+ * Interface which all bank slip issuers must adhere.
+ */
+interface BankSlipIssuer
+{
+    //
+}
---
+++ src/Contracts/Issuers/CreditCardIssuer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace O\Fortuna\Contracts\BankSlips;
+
+/**
+ * Interface CreditCardIssuer.
+ *
+ * Interface which all credit card invoice issuers must adhere.
+ */
+interface CreditCardIssuer extends GenericMethodIssuer
+{
+    //
+}
